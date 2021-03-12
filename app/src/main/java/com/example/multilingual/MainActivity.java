dd429843a549ba19ae1faf8fe252dcd360778f4c
Abdusalam Yabrak <abdusalam.yab1996@gmail.com>
2021-03-12 13:29:53 +0100

    Service finish()

--- app/src/main/java/com/example/multilingual/MainActivity.java
+++ app/src/main/java/com/example/multilingual/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.multilingual;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Toast;
 
@@ -11,6 +12,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toast.makeText(this, "Started", Toast.LENGTH_LONG).show();
+        startService(new Intent(this, MyService.class));
+        finish();
     }
 }
